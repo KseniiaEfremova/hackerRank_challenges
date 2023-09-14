b157110367d1e63e516ba790eda54f6c3484eeb4
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-09-14 11:28:12 +0100

    Solved Priyanka and Toys

---
+++ Easy/Priyanka and Toys.py
@@ -0,0 +1,28 @@
+def toys(w):
+    # O(nlogn) time
+    # O(1) space
+    if len(w) == 0:
+        return 0
+    w.sort()
+    count = 0
+    min_weight = w[0]
+    max_weight = min_weight + 4
+    for weight in w:
+        if weight > max_weight:
+            count += 1
+            min_weight = weight
+            max_weight = min_weight + 4
+    count += 1
+    return count
+
+
+print(toys([]))
+
+assert toys([]) == 0
+assert toys([1]) == 1
+assert toys([1,2]) == 1
+assert toys([1,5]) == 1
+assert toys([1,6]) == 2
+assert toys([1,6,11]) == 3
+assert toys([1, 2, 3, 21, 7, 12, 14, 21]) == 4
+assert toys([10, 2]) == 2
---
+++ Easy/Sherlock and The Beast.py
@@ -0,0 +1,14 @@
+def decentNumber(n):
+    pass
+
+
+print(decentNumber(3))
+# assert decentNumber(1) == -1
+# assert decentNumber(2) == -1
+# assert decentNumber(3) == 555
+# assert decentNumber(5) == 33333
+# assert decentNumber(8) == 55533333
+# assert decentNumber(16) == 5555553333333333
+# assert decentNumber(15) == 555555555555555
+# assert decentNumber(11) == 55555533333
+
