9257af69aa1c0ef7d9f5f5f010518c7161a852cb
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-09-14 12:20:47 +0100

    Solved Sherlock and The Beast

--- Easy/Sherlock and The Beast.py
+++ Easy/Sherlock and The Beast.py
@@ -1,14 +1,21 @@
 def decentNumber(n):
-    pass
+    m = n
+    while m % 3 != 0:
+        m -= 5
+
+    if m < 0:
+        print(-1)
+    else:
+        print("5" * m, "3"*(n-m), sep="")
 
 
 print(decentNumber(3))
-# assert decentNumber(1) == -1
-# assert decentNumber(2) == -1
-# assert decentNumber(3) == 555
-# assert decentNumber(5) == 33333
-# assert decentNumber(8) == 55533333
-# assert decentNumber(16) == 5555553333333333
-# assert decentNumber(15) == 555555555555555
-# assert decentNumber(11) == 55555533333
+# assert decentNumber(1) == "-1"
+# assert decentNumber(2) == "-1"
+# assert decentNumber(3) == "555"
+# assert decentNumber(5) == "33333"
+# assert decentNumber(8) == "55533333"
+# assert decentNumber(16) == "5555553333333333"
+# assert decentNumber(15) == "555555555555555"
+# assert decentNumber(11) == "55555533333"
 
