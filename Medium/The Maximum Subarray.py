c6933d8e5ee77e7e83215b791043031ef98af3f5
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-10-25 16:08:27 +0100

    Solved

---
+++ Medium/The Maximum Subarray.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def maxSubsequences(arr):
+    # n - len(arr)
+    # O(n) time
+    # O(n) space
+    max_subsequences = 0
+    for num in arr:
+        if num > 0:
+            max_subsequences += num
+    if not max_subsequences:
+        return max(arr)
+    return max_subsequences
+
+
+def maxSubarray(arr):
+    # n - len(arr)
+    # O(n**2) time
+    # O(n) space
+    len_subarray = 1
+    max_subarray = sum(arr)
+    while len_subarray <= len(arr):
+        for i in range(len(arr) - len_subarray + 1):
+            temp_subarray = arr[i: i + len_subarray]
+            max_subarray = max(max_subarray, sum(temp_subarray))
+        len_subarray += 1
+
+    return [max_subarray, maxSubsequences(arr)]
+
+
+print(maxSubarray([-2, -3, -1, -4, -6]))
+
+assert maxSubarray([-1,2,3,-4,5,10]) == [16,20]
+assert maxSubarray([1,2,3,4]) == [10, 10]
+assert maxSubarray([2, -1, 2, 3, 4, -5]) == [10, 11]
+assert maxSubarray([-2, -3, -1, -4, -6]) == [-1, -1]
+assert maxSubarray([1, -100, 3]) == [3, 4]
+
