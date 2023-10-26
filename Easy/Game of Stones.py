890d1e1885760e6d794a75b70f0e72b731633397
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-10-26 13:17:54 +0100

    Improve TC O(n) to O(1)

--- Easy/Game of Stones.py
+++ Easy/Game of Stones.py
@@ -1,34 +1,13 @@
 # True - first wins
 # False - second wins
 def gameOfStones(n):
-    # O(n) time
-    # O(1) space
-    res = [False, False, True, True, True]
-    if n < len(res):
-        if res[n]:
-            return "First"
-        return "Second"
-    moves = [3, 2, 0]
-    for k in range(5, n+1):
-        has_win_moves = False
-        for move in moves:
-            if not res[move]:
-                has_win_moves = True
-                break
-        res.append(has_win_moves)
-        res.pop(0)
-    if res[-1]:
+    # O(1) time
+    # O(len(res)) space
+    res = [False, False, True, True, True, True, True]
+    if res[n % 7]:
         return "First"
     return "Second"
 
-
-def gameOfStones2(n):
-    if n % 7 < 2:
-        return "Second"
-    return "First"
-
-
-print(gameOfStones(9))
 
 assert gameOfStones(1) == "Second"
 assert gameOfStones(7) == "Second"
