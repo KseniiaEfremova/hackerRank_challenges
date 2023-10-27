a36c5d7254de32a2a715088b5cabbd3c68d7f172
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-10-27 11:42:12 +0100

    Solved

--- Easy/The Coin Change Problem.py
+++ Easy/The Coin Change Problem.py
@@ -1,2 +1,11 @@
 def getWays(n, c):
- pass
+    # m - len(c)
+    # O(n*m) time
+    # O(n) space
+    ways = [0 for _ in range(n+1)]
+    ways[0] = 1
+    for coin in c:
+        for amount in range(n+1):
+            if amount >= coin:
+                ways[amount] += ways[amount-coin]
+    return ways[n]
