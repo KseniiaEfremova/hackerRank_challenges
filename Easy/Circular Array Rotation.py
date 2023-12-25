8beba884907e36518f97f2a1c4e20601ca48730d
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-12-25 15:03:44 +0000

    Solved

--- Easy/Circular Array Rotation.py
+++ Easy/Circular Array Rotation.py
@@ -1,13 +1,13 @@
 def circularArrayRotation(a, k, queries):
-    # TC O(n), n - len(a)
-    # SC O(n), n - len(a)
+    # TC O(n), n - len(queries)
+    # SC O(n), n - len(queries)
     n = len(a)
     k = k % n
-    rotated_array = [0 for _ in a]
-    for i in range(n):
-        rotated_indx = (i + k) % n
-        rotated_array[rotated_indx] = a[i]
-    return [rotated_array[number] for number in queries]
+    res = []
+    for num in queries:
+        rotated_indx = (num - k) % n
+        res.append(a[rotated_indx])
+    return res
 
 
 assert circularArrayRotation([1,2,3,4],2,[0,1]) == [3, 4]
@@ -15,5 +15,3 @@ assert circularArrayRotation([1,2,3,4],1,[0,1]) == [4, 1]
 assert circularArrayRotation([1,2,3,4],3,[0,1]) == [2, 3]
 assert circularArrayRotation([1,2,3,4],4,[0,1]) == [1, 2]
 assert circularArrayRotation([1,2,3,4],5,[0,1]) == [4, 1]
-
-
--- Easy/Missing Numbers.py
+++ Easy/Missing Numbers.py
@@ -1,5 +1,5 @@
 def missingNumbers(arr, brr):
-    # TC O(n), n - len(brr)
+    # TC O(nlogn), n - len(brr)
     # SC O(n), n - len(brr)
     numbers = {}
     for number in brr:
@@ -11,4 +11,5 @@ def missingNumbers(arr, brr):
 
 
 assert missingNumbers([1,2,2,3], [1,1,2,2,3,3]) == [1,3]
-assert missingNumbers([203, 204, 205, 206, 207, 208, 203, 204, 205, 206], [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204]) == [204, 205, 206]
+assert missingNumbers([203, 204, 205, 206, 207, 208, 203, 204, 205, 206],
+                      [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204]) == [204, 205, 206]
